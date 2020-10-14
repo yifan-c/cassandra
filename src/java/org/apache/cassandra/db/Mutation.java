12366daca366f9685ca01ad29a6e1e861e9953bf
Marcus Eriksson <marcuse@apache.org>
2020-10-14 17:02:17 +0200

    avoid System.currentTimeMillis for every mutation
    Benchmark                                                 (batchSize)  (uniquePartition)   Mode  Samples     Score   Error   Units
    o.a.c.t.m.BatchStatementBench.bench                             10000               true  thrpt        5     0.188 ± 0.013  ops/ms
    o.a.c.t.m.BatchStatementBench.bench:@gc.count.profiled          10000               true  thrpt        5   814.000 ±   NaN  counts
    o.a.c.t.m.BatchStatementBench.bench:@gc.count.total             10000               true  thrpt        5   898.000 ±   NaN  counts
    o.a.c.t.m.BatchStatementBench.bench:@gc.time.profiled           10000               true  thrpt        5  1354.000 ±   NaN      ms
    o.a.c.t.m.BatchStatementBench.bench:@gc.time.total              10000               true  thrpt        5  1522.000 ±   NaN      ms
    o.a.c.t.m.BatchStatementBench.bench                             10000              false  thrpt        5     0.282 ± 0.019  ops/ms
    o.a.c.t.m.BatchStatementBench.bench:@gc.count.profiled          10000              false  thrpt        5   729.000 ±   NaN  counts
    o.a.c.t.m.BatchStatementBench.bench:@gc.count.total             10000              false  thrpt        5   804.000 ±   NaN  counts
    o.a.c.t.m.BatchStatementBench.bench:@gc.time.profiled           10000              false  thrpt        5   907.000 ±   NaN      ms
    o.a.c.t.m.BatchStatementBench.bench:@gc.time.total              10000              false  thrpt        5  1027.000 ±   NaN      ms

--- src/java/org/apache/cassandra/db/Mutation.java
+++ src/java/org/apache/cassandra/db/Mutation.java
@@ -57,7 +57,7 @@ public class Mutation implements IMutation
     private final Map<UUID, PartitionUpdate> modifications;
 
     // Time at which this mutation was instantiated
-    public final long createdAt = System.currentTimeMillis();
+    public final long createdAt;
     // keep track of when mutation has started waiting for a MV partition lock
     public final AtomicLong viewLockAcquireStart = new AtomicLong(0);
 
@@ -68,6 +68,11 @@ public class Mutation implements IMutation
         this(keyspaceName, key, new HashMap<>());
     }
 
+    public Mutation(String keyspaceName, DecoratedKey key, long createdAt)
+    {
+        this(keyspaceName, key, new HashMap<>(), createdAt);
+    }
+
     public Mutation(PartitionUpdate update)
     {
         this(update.metadata().ksName, update.partitionKey(), Collections.singletonMap(update.metadata().cfId, update));
@@ -75,11 +80,17 @@ public class Mutation implements IMutation
 
     protected Mutation(String keyspaceName, DecoratedKey key, Map<UUID, PartitionUpdate> modifications)
     {
+        this(keyspaceName, key, modifications, System.currentTimeMillis());
+    }
+
+    private Mutation(String keyspaceName, DecoratedKey key, Map<UUID, PartitionUpdate> modifications, long createdAt)
+    {
         this.keyspaceName = keyspaceName;
         this.key = key;
         this.modifications = modifications;
         for (PartitionUpdate pu : modifications.values())
             cdcEnabled |= pu.metadata().params.cdc;
+        this.createdAt = createdAt;
     }
 
     public Mutation copy()
