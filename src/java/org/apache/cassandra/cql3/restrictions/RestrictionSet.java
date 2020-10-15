5ac547905eaad01f799296f0d6094da261b6b86e
Marcus Eriksson <marcuse@apache.org>
2020-10-15 12:10:17 +0200

    avoid copying the restrictions
    Benchmark                                                 (batchSize)  (uniquePartition)   Mode  Samples     Score   Error   Units
    o.a.c.t.m.BatchStatementBench.bench                             10000               true  thrpt        5     0.096 ± 0.005  ops/ms
    o.a.c.t.m.BatchStatementBench.bench:@gc.count.profiled          10000               true  thrpt        5   630.000 ±   NaN  counts
    o.a.c.t.m.BatchStatementBench.bench:@gc.count.total             10000               true  thrpt        5   695.000 ±   NaN  counts
    o.a.c.t.m.BatchStatementBench.bench:@gc.time.profiled           10000               true  thrpt        5   894.000 ±   NaN      ms
    o.a.c.t.m.BatchStatementBench.bench:@gc.time.total              10000               true  thrpt        5  1047.000 ±   NaN      ms
    o.a.c.t.m.BatchStatementBench.bench                             10000              false  thrpt        5     0.183 ± 0.008  ops/ms
    o.a.c.t.m.BatchStatementBench.bench:@gc.count.profiled          10000              false  thrpt        5   821.000 ±   NaN  counts
    o.a.c.t.m.BatchStatementBench.bench:@gc.count.total             10000              false  thrpt        5   907.000 ±   NaN  counts
    o.a.c.t.m.BatchStatementBench.bench:@gc.time.profiled           10000              false  thrpt        5  1000.000 ±   NaN      ms
    o.a.c.t.m.BatchStatementBench.bench:@gc.time.total              10000              false  thrpt        5  1153.000 ±   NaN      ms

--- src/java/org/apache/cassandra/cql3/restrictions/RestrictionSet.java
+++ src/java/org/apache/cassandra/cql3/restrictions/RestrictionSet.java
@@ -251,6 +251,6 @@ final class RestrictionSet implements Restrictions, Iterable<Restriction>
     @Override
     public Iterator<Restriction> iterator()
     {
-        return new LinkedHashSet<>(restrictions.values()).iterator();
+        return restrictions.values().iterator();
     }
 }
